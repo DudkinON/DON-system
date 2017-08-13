25d935576c834d000a9ca7b608a546ec444d14f3
DudkinON <sumonux@gmail.com>
2017-08-12 20:43:32 -0500

    deleted the old files

--- don/views/install/sidebar.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by Oleg Dudkin.
- * Project: CMS "DON e-Commerce"
- * Date: 7/29/2017
- * Time: 7:42 PM
- */
--- template/default/js/main.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function ($) {
-    $('.nav-slider-btn').click(function () {
-        $('.nav-list-container').toggle();
-    });
-});
